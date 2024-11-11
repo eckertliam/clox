58e1d42c23c899dc639ee6d85b1f0cddc75317a1
Liam Eckert <liameckert17@gmail.com>
2024-11-11 06:44:55 -0500

    worked to 15.2.1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clox
 My implementation of Clox from https://craftinginterpreters.com/
+
+## Continue From
+15.2.1
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main() {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
@@ -10,6 +12,7 @@ int main() {
     writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
